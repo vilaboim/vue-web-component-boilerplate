bf4311faf068219063d61de50f8bcd38ffd9d5ba
Lucas Vilaboim <lucas@vilaboim.com>
2019-07-16 12:08:09 -0300

    fix(deps): update clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const {
   fileToComponentName,
   resolveEntry
